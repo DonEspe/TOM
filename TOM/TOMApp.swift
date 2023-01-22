6278c8e11950561c1f506943a55f77a4eba92472
Don Espe <Don.Espe@gmail.com>
2023-01-21 23:07:50 -0600

    Initial Commit

---
+++ TOM/TOMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOMApp.swift
+//  TOM
+//
+//  Created by Don Espe on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TOMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
